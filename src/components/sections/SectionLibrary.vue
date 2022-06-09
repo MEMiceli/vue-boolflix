155f10835ba4daf0a1849492f2b59b762b797598
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-06-09 17:30:11 +0200

    letsgo

---
+++ src/components/sections/SectionLibrary.vue
@@ -0,0 +1,32 @@
+<template>
+    <section>
+        <div class="container">
+            <div class="row ">
+                <SearchBar @searching="filtergenre"/>
+                <AlbumItem class="col-12 col-sm-6 col-lg-3 mt-5" v-for="album in albumsearched" :key="album.index" :album="album"/>
+            </div> 
+        </div>
+    </section>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: 'SectionLibrary',
+    data(){
+        return{
+            films:[],
+        }
+    },
+    components:{
+
+    },
+    created()
+    
+
+}
+</script>
+
+<style>
+
+</style>
