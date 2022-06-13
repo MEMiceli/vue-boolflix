297ddae66b7dfe321d98a64b94c7e6eab33eda43
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-06-13 12:49:42 +0200

    rompendo tutto, anche i santi

--- src/components/commons/SearchBar.vue
+++ src/components/commons/SearchBar.vue
@@ -1,18 +1,44 @@
 <template>
-    <div class="input-group rounded">
-        <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-        <span class="input-group-text border-0" id="search-addon">
-        <i class="fas fa-search"></i>
-        </span>
-</div>
+    <form class="input-group" @submit.prevent="searching">
+        <input type="text" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" v-model="searchText" required/>
+        <button type="submit" class="btn btn-outline-primary">search</button>
+    </form>
 </template>
 
 <script>
+import axios from 'axios';
+import data from '../../shared/data';
 export default {
+    name:'SearchBar',
+    data(){
+        return{
+            data,
+            searchText:'',
+        }
+    },
+    methods:{
+        searching(){
+            axios.get('https://api.themoviedb.org/3/search/movie',{
+                params:{
+                    api_key:'093da68c34711cf715f097616d53b2ae',
+                    query: this.searchText,
+                    language: 'it-IT',
+                }
+            }).then((response) => {
+                data.films = response.data.results;
+                this.searchText='';
+                console.log(response.data.results);
+            }).catch((error) => {
+            console.log(error);
+            })
+
+
+        }
+    }
 
 }
 </script>
 
-<style>
+<style scoped lang="scss">
 
 </style>
--- src/components/sections/SectionLibrary.vue
+++ src/components/sections/SectionLibrary.vue
@@ -3,45 +3,44 @@
         <div class="container">
             <div class="row ">
                 <SearchBar/>
-                <div class="col-12 col-sm-6 col-lg-3 mt-5" v-for="film in films" :key="film.index">
+                <div>
                     <ul>
-                        <li>{{film.title}}</li>
-                        <li>{{film.original_title}}</li>
-                        <li>{{film.original_language}}</li>
-                        <li>{{film.vote_average}}</li>
-                    </ul>
+                        <li class="col-12 col-sm-6 col-lg-3 mt-5" v-for="film in films.data" :key="film.index">
+                        <ul>
+                            <li>Titolo:{{film.title}}</li>
+                            <li>Titolo Originale:{{film.original_title}}</li>
+                            <li>Lingua Originale{{film.original_language}}</li>
+                            <li>Voto{{film.vote_average}}</li>
+                        </ul>
+                    </li>
+                </ul>
                 </div>
+                
+                
             </div> 
         </div>
     </section>
 </template>
 
 <script>
-import axios from 'axios';
+
 import SearchBar from '../commons/SearchBar.vue'
+import data from '../../shared/data'
 export default {
     name: 'SectionLibrary',
     data(){
         return{
-            films:[],
+            data,
+           
         }
     },
     components:{
         SearchBar,
     },
-    created(){
-        axios.get('https://api.themoviedb.org/3/search/movie',{
-        params:{
-            api_key:'093da68c34711cf715f097616d53b2ae',
-            query:'attacco',
-            language: 'it-IT',
-        }
-        }).then((response) => {
-            this.films = response.data.results;
-        }).catch((error) => {
-            console.log(error);
-        })
-    }
+    methods:{
+    
+       
+    },
     
 
 }
