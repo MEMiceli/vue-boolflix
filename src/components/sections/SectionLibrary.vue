068c47e79ecb9ad1359b06b68fc09bd31800b8c1
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-06-13 13:57:29 +0200

    rompendo si impara

--- src/components/sections/SectionLibrary.vue
+++ src/components/sections/SectionLibrary.vue
@@ -3,20 +3,18 @@
         <div class="container">
             <div class="row ">
                 <SearchBar/>
-                <div>
-                    <ul>
-                        <li class="col-12 col-sm-6 col-lg-3 mt-5" v-for="film in films.data" :key="film.index">
-                        <ul>
-                            <li>Titolo:{{film.title}}</li>
-                            <li>Titolo Originale:{{film.original_title}}</li>
-                            <li>Lingua Originale{{film.original_language}}</li>
-                            <li>Voto{{film.vote_average}}</li>
-                        </ul>
-                    </li>
-                </ul>
+                <div class="container">
+                    <ul class="row">
+                        <li class="col-12 col-sm-6 col-lg-3 mt-5" v-for="film in data.films" :key="film.index">
+                            <ul>
+                                <li>Titolo:{{film.title}}</li>
+                                <li>Titolo Originale:{{film.original_title}}</li>
+                                <li>Lingua Originale{{film.original_language}}</li>
+                                <li>Voto{{film.vote_average}}</li>
+                            </ul>
+                        </li>
+                    </ul>
                 </div>
-                
-                
             </div> 
         </div>
     </section>
